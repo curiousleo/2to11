3ba9a049cda0a65540f8afb0bf90d27b5012a3b0
Leonhard Markert <curiousleo@ymail.com>
2014-07-10 08:54:04 +0200

    Field to Board, guard

--- library/Game.hs
+++ library/Game.hs
@@ -2,10 +2,12 @@
 -- | The game logic for 2048.
 module Game (
       Direction (..)
-    , Field (..)
+    , Board (..)
     , Position
     , State
     , Value
+
+    , emptyBoard
 
     , playComputer
 
@@ -22,7 +24,7 @@ module Game (
     ) where
 
 import Control.Arrow (first)
-import Control.Monad (mzero)
+import Control.Monad (mzero, guard)
 import Control.Monad.Trans.Maybe (MaybeT)
 import Control.Monad.Trans.Class (lift)
 
@@ -30,7 +32,7 @@ import qualified Data.Vector as V
 import System.Random (getStdRandom, randomR)
 
 -- | Models the 2048 game field as a vector of int vectors.
-newtype Field = Field { unField :: V.Vector (V.Vector Value) }
+newtype Board = Board { unBoard :: V.Vector (V.Vector Value) }
     deriving Eq
 type Value = Int
 
@@ -38,7 +40,7 @@ type Value = Int
 type Position = (Int, Int)
 
 -- | The state, modelled as the game field and the current score.
-type State = (Field, Int)
+type State = (Board, Int)
 
 -- | A player's move.
 data Direction = R | U | L | D
@@ -56,17 +58,18 @@ move D = first rot270 . move' . first rot90
 -- | Computer places a 2 or 4 on a free space.
 place :: Position   -- ^ The position of the newly placed number
       -> Value      -- ^ What value to place
-      -> Field      -- ^ The current state
-      -> Field      -- ^ The next state
-place (r, c) val = Field . place' . unField where
+      -> Board      -- ^ The current state
+      -> Board      -- ^ The next state
+place (r, c) val = Board . place' . unBoard where
     place' vec = vec V.// [(r, vec V.! r V.// [(c, val)])]
 
 playComputer :: State -> MaybeT IO State
 playComputer (field, score) = do
     field' <- placeRandom field
+    guard $ null . possibleMoves $ field
     return (field', score)
 
-placeRandom :: Field -> MaybeT IO Field
+placeRandom :: Board -> MaybeT IO Board
 placeRandom field = do
     pos <- choose $ freePositions field
     val <- choose [1, 2]
@@ -80,34 +83,34 @@ choose xs = do
     return $ xs !! i
 
 -- | Can the player move in the given direction?
-canMove :: Direction -> Field -> Bool
+canMove :: Direction -> Board -> Bool
 canMove dir field = 0 /= score where
     score = snd $ move dir (field, 0)
 
 -- | Which moves are possible?
-possibleMoves :: Field -> [Direction]
+possibleMoves :: Board -> [Direction]
 possibleMoves field = filter (flip canMove field) [R, U, L, D]
 
 -- | Can the computer put a new number at the given position?
-free :: Position -> Field -> Bool
-free (r, c) = (== 0) . (\vec -> (vec V.! r) V.! c) . unField
+free :: Position -> Board -> Bool
+free (r, c) = (== 0) . (\vec -> (vec V.! r) V.! c) . unBoard
 
 -- | Which positions are still free?
-freePositions :: Field -> [Position]
+freePositions :: Board -> [Position]
 freePositions field = filter (flip free field) positions where
     positions = [ (i,j) | i <- [0 .. r-1], j <- [0 .. c-1] ]
     (r, c) = dimensions field
 
 move' :: State -> State
-move' (field, score) = (Field vec', score + V.sum scores) where
+move' (field, score) = (Board vec', score + V.sum scores) where
     (_, w) = dimensions field
-    combined = V.map (first (restore w) . compact) (unField field)
+    combined = V.map (first (restore w) . compact) (unBoard field)
     (vec', scores) = V.unzip combined
 
-rot90, rot180, rot270 :: Field -> Field
-rot90 field = Field rotated where
+rot90, rot180, rot270 :: Board -> Board
+rot90 field = Board rotated where
     rotated = V.generate w (\i -> V.generate h (\j -> rot90' i j))
-    rot90' i j = ((unField field) V.! j) V.! (w-i-1)
+    rot90' i j = ((unBoard field) V.! j) V.! (w-i-1)
     (h, w) = dimensions field
 rot180 = rot90 . rot90
 rot270 = rot90 . rot90 . rot90
@@ -127,29 +130,27 @@ compact = first (V.fromList . dropWhile (== 0)) . V.foldr f ([], 0) where
 restore :: Int -> V.Vector Value -> V.Vector Value
 restore n vec = V.replicate (n - V.length vec) 0 V.++ vec
 
-dimensions :: Field -> (Int, Int)
+dimensions :: Board -> (Int, Int)
 dimensions field
     | V.null vec = (0, 0)
     | otherwise  = (V.length vec, V.length (vec V.! 0))
     where
-        vec = unField field
+        vec = unBoard field
 
-initial :: Int      -- ^ Width of the game field
-        -> Int      -- ^ Height of the game field
-        -> Field    -- ^ Generated game field
-initial x y = Field $ V.generate x (const V.replicate y 0)
-
-example :: Field
-example = Field $ V.fromList (map V.fromList m) where
+example :: Board
+example = Board $ V.fromList (map V.fromList m) where
     m = [[0, 1, 0, 1, 2],
          [0, 1, 1, 2, 3],
          [1, 1, 2, 2, 2]]
 
-instance Show Field where
-    show = showField
+emptyBoard :: Position -> Board
+emptyBoard (r, c) = Board $ V.replicate r $ V.replicate c 0
 
-showField :: Field -> String
-showField = V.foldl (\s -> \v -> s ++ ('\n' : (showRow v))) "" . unField where
+instance Show Board where
+    show = showBoard
+
+showBoard :: Board -> String
+showBoard = V.foldl (\s -> \v -> s ++ ('\n' : (showRow v))) "" . unBoard where
     showRow = concat . V.toList . V.map showNum
     showNum 0 = replicate 5 ' ' ++ "0"
     showNum x = let s = show ((2::Value)^x) in (replicate (6 - length s) ' ') ++ s

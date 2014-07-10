de2263141c350394ef1294b339b77ff0869bd97d
Leonhard Markert <curiousleo@ymail.com>
2014-07-10 10:47:23 +0200

    test runner

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,12 +1,34 @@
 module Main (main) where
 
-import Control.Monad ((<=<))
-import Control.Monad.Trans.Maybe (runMaybeT)
+import Control.Monad ((<=<), liftM)
+import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
 
 import Game
 import AI
 
+play :: (State -> MaybeT IO State) -> IO State
+play ai = last `liftM` do
+    (Just start) <- runMaybeT initial
+    unfoldr' (runMaybeT . next ai) start
+
+next :: (State -> MaybeT IO State) -> State -> MaybeT IO State
+next ai s = (playComputer <=< ai) =<< (MaybeT . return . return $ s)
+
+initial :: MaybeT IO State
+initial = playComputer <=< playComputer $ (emptyBoard (5, 5), 0)
+
 main :: IO ()
 main = do
-    init <- runMaybeT $ playComputer <=< playComputer $ (emptyBoard (5, 5), 0)
-    print $ init
+    let n = 100
+    scores <- mapM play $ replicate n playAI'
+    print $ (fromIntegral . sum . snd . unzip $ scores) / (fromIntegral n)
+
+unfoldr' :: Monad m => (a -> m (Maybe a)) -> a -> m [a]
+unfoldr' f x = do
+    maybe_x' <- f x
+    case maybe_x' of
+        Just x' -> do
+            l <- unfoldr' f x'
+            return $ x' : l
+        Nothing ->
+            return []

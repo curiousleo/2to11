f94f49fa1ae68aaba54ef1835773e4bde4841da5
Leonhard Markert <curiousleo@ymail.com>
2014-07-10 08:54:30 +0200

    executable needs transformers

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,7 +1,12 @@
 module Main (main) where
 
+import Control.Monad ((<=<))
+import Control.Monad.Trans.Maybe (runMaybeT)
+
 import Game
 import AI
 
 main :: IO ()
-main = print ()
+main = do
+    init <- runMaybeT $ playComputer <=< playComputer $ (emptyBoard (5, 5), 0)
+    print $ init

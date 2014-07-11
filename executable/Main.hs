2da2425ba02f2dd522f3ff26d97ad41c04bb265a
Leonhard Markert <curiousleo@ymail.com>
2014-07-11 10:27:42 +0200

    use play

--- executable/Main.hs
+++ executable/Main.hs
@@ -5,8 +5,8 @@ import AI
 main :: IO ()
 main = do
     let n = 1000
-    let ev ai = runEval (4,4) ai eval
-    scores <- mapM ev $ replicate n baselineAI
+    let dims = (4, 4)
+    scores <- mapM (play dims) $ replicate n baselineAI
     print $ stats scores
 
 stats :: [Int] -> (Int, Double, Int)

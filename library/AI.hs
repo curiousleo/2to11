d19c39973c1e9d1adfffdcf6bb09ae75379a5ce1
Leonhard Markert <curiousleo@ymail.com>
2014-07-11 10:27:26 +0200

    runStateT instead of evalStateT

--- library/AI.hs
+++ library/AI.hs
@@ -1,23 +1,31 @@
 -- library/AI.hs
 -- | The AI player for 2048.
 module AI (
-      baselineAI
+      AI
+    , Eval
+
+    , play
+
     , runEval
     , eval
+
+    , baselineAI
     ) where
 
 import Prelude hiding (mapM)
 
-import Control.Monad ((<=<))
+import Control.Monad ((<=<), liftM)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
-import Control.Monad.Trans.State (StateT, evalStateT, get, put)
+import Control.Monad.Trans.State (StateT, runStateT, get, put)
 
 import Data.List (find)
 import Data.Maybe (fromMaybe)
 
 import Game
 
+-- | The type of an AI policy, mapping the state (of the board) to an action (a
+--   move in a certain direction).
 type AI = Board -> Direction
 
 -- | The evaluation monad.
@@ -25,30 +33,35 @@ type Eval = ReaderT AI (StateT GameState IO)
 
 -- | Evaluate an AI on a board of given dimensions.
 --
---   Example:
---
---   >>> runEval (4, 4) baselineAI eval
---   3067
-runEval :: Dimensions -> AI -> Eval a -> IO a
-runEval dims ai ev = do
+--   >>> runEval eval (4, 4) baselineAI
+--   ((),(
+--        2    32     8   256
+--        4    64    32    64
+--       16     8    16    32
+--        2     4     8    16,2932))
+runEval :: Eval a -> Dimensions -> AI -> IO (a, GameState)
+runEval ev dims ai = do
     startState <- playComputer <=< playComputer $ (emptyBoard dims, 0)
-    runEval' startState ai ev
+    runEval' ev startState ai
 
 -- | Evaluate an AI given a starting state.
-runEval' :: GameState -> AI -> Eval a -> IO a
-runEval' st ai ev = evalStateT (runReaderT ev ai) st
+runEval' :: Eval a -> GameState -> AI -> IO (a, GameState)
+runEval' ev st ai = runStateT (runReaderT ev ai) st
 
--- | Play a game, return final score
-eval :: Eval Score
+-- | Play a game to the end.
+eval :: Eval ()
 eval = do
     ai <- ask
-    state@(board, score) <- lift get
+    state@(board, _) <- lift get
     if null $ possibleMoves board
-        then return score
+        then return ()
         else do
             state' <- lift . lift . playComputer $ playAI ai state
             lift . put $ state'
             eval
+
+play :: Dimensions -> AI -> IO Score
+play = liftM (liftM (snd . snd)) . runEval eval
 
 -- | Very simple AI player: chooses the first of Up, Right, Down that is
 --   possible (using `canMove`); otherwise plays Left.
